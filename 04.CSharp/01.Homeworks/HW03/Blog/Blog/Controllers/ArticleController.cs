e013656c51fc7bb5fc5a7bf2c5d4b933fc3c68d7
Tihomir Dimov <tihomirdimov@tihomirdimov.com>
2017-08-13 01:01:15 +0300

    HW03 completed

---
+++ 04.CSharp/01.Homeworks/HW03/Blog/Blog/Controllers/ArticleController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            using (var db = new BlogDbContext())
+            {
+                var articles = db.Articles.Include(a => a.Author).ToList();
+                return View(articles);
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles.Where(a => a.Id == id).Include(a => a.Author).First();
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(article);
+            }
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new BlogDbContext())
+                {
+                    var authorId = db.Users
+                        .Where(u => u.UserName == this.User.Identity.Name)
+                        .First()
+                        .Id;
+                    article.AuthorId = authorId;
+                    db.Articles.Add(article);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(article);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(article);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles.Where(a => a.Id == id).Include(a => a.Author).First();
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+                db.Articles.Remove(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var db = new BlogDbContext())
+            {
+                var article = db.Articles.Where(a => a.Id == id).First();
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+                var model = new ArticleViewModel();
+
+                model.Id = article.Id;
+                model.Title = article.Title;
+                model.Content = article.Content;
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ArticleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new BlogDbContext())
+                {
+                    var article = db.Articles.FirstOrDefault(a => a.Id == model.Id);
+                    if (article == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    article.Title = model.Title;
+                    article.Content = model.Content;
+
+                    db.Entry(article).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+
+        private bool IsUserAuthorizedToEdit(Article article)
+        {
+            bool isAdmin = this.User.IsInRole("Admin");
+            bool isAuthor = article.IsAuthor(this.User.Identity.Name);
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
