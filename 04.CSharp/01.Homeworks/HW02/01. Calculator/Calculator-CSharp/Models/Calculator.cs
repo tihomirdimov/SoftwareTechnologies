9570e9732c54c0d9acbc69d1e8f5b57b20e39920
Tihomir Dimov <tihomirdimov@tihomirdimov.com>
2017-08-12 17:37:46 +0300

    Software Technologies C# HW02 Task 01

---
+++ 04.CSharp/01.Homeworks/HW02/01. Calculator/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            this.Result = 0;
+        }
+        public Decimal LeftOperand { get; set; }
+        public Decimal RightOperand { get; set; }
+        public String Operator { get; set; }
+        public Decimal Result { get; set; }
+
+    }
+}
