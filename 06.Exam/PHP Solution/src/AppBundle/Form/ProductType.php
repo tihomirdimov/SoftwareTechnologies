e16031e4f93ec0238f3bc7928d87191dec6d8a4f
Tihomir Dimov <tihomirdimov@tihomirdimov.com>
2017-09-02 21:53:12 +0300

    exam

---
+++ 06.Exam/PHP Solution/src/AppBundle/Form/ProductType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        //TODO: Implement me ...
+
+        $builder
+            ->add('priority', TextType::class)
+            ->add('name', TextType::class)
+            ->add('quantity', TextType::class)
+            ->add('status',  TextType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Product'
+        ));
+    }
+}
